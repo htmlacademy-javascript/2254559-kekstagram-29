dafc84d3d3eac9e326a324a6798e1b460c972493
Александр Головко <a-golovko2009@yandex.ru>
2023-07-16 15:42:21 +0500

    добавляет валидность кол-ва хештегов

--- js/form.js
+++ js/form.js
@@ -1,49 +1,95 @@
-const imgUploadForm = document.querySelector('.img-upload__form');
-const imgUploadField = imgUploadForm.querySelector('.img-upload__input');
-const imgUploadWindow = imgUploadForm.querySelector('.img-upload__overlay');
-const closeButtonForm = imgUploadWindow.querySelector('.img-upload__cancel');
-const hashtagsField = imgUploadWindow.querySelector('.text__hashtags');
-const descriptionField = imgUploadWindow.querySelector('.text__description');
+const MAX_COUNT_HASHTAGS = 5;
+
+const VALID_HASHTAG_ERROR_MESSAGE = 'Введен невалидный хештег';
+const UNIQ_HASHTAG_ERROR_MESSAGE = 'Хештеги не должны повторяться';
+const COUNT_HASHTAG_ERROR_MESSAGE = 'Максимальное количество хештегов - 5';
+
+const form = document.querySelector('.img-upload__form');
+const imgInputFieldForm = form.querySelector('.img-upload__input');
+const modalForm = form.querySelector('.img-upload__overlay');
+const closeButton = modalForm.querySelector('.img-upload__cancel');
+const hashtagsField = modalForm.querySelector('.text__hashtags');
+const descriptionField = modalForm.querySelector('.text__description');
 
 
 //функция открытия окна формы загрузки фотографии
-const openUploadForm = () => {
-  imgUploadWindow.classList.remove('hidden');
+const openForm = () => {
+  modalForm.classList.remove('hidden');
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
+  hashtagsField.addEventListener('keydown', onTextFieldKeydown);
+  descriptionField.addEventListener('keydown', onTextFieldKeydown);
 };
 
 //функция закрытия окна формы
-const closeUploadForm = () => {
-  imgUploadWindow.classList.add('hidden');
+const closeForm = () => {
+  modalForm.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
-  imgUploadField.value = '';
-  hashtagsField.value = '';
-  descriptionField.value = '';
+  hashtagsField.removeEventListener('keydown', onTextFieldKeydown);
+  descriptionField.removeEventListener('keydown', onTextFieldKeydown);
+  imgInputFieldForm.value = '';
+  form.reset();
+  pristine.reset();
 };
 
 //обработчик открытия формы
-imgUploadField.addEventListener('change', openUploadForm);
+const onInputFieldChange = () => openForm();
+imgInputFieldForm.addEventListener('change', onInputFieldChange);
 
 //обработчик закрытия окна формы
-closeButtonForm.addEventListener('click', closeUploadForm);
+const onCloseButtonClick = () => closeForm();
+closeButton.addEventListener('click', onCloseButtonClick);
 
 //функция закрятия по нажатию ESC
 function onDocumentKeydown (evt) {
   evt.preventDefault();
   if (evt.key === 'Escape') {
-    closeUploadForm();
+    closeForm();
+  }
+}
+
+//функция отмены закрытия модального окна, при фокусе полей хештег, комментарий
+function onTextFieldKeydown (evt) {
+  if (evt.key === 'Escape') {
+    evt.stopPropagation();
   }
 }
 
 //валидация формы
-const pristine = new Pristine(imgUploadForm, {
+const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
   errorTextOarent: 'img-upload__field-wrapper',
 });
 
-imgUploadForm.addEventListener('submit', (evt) => {
+//подготовка текстовой строки хештега к проверке
+const normalizeHashtag = (textHashtag) => {
+  textHashtag.trim().split(' ').filter((tag) => Boolean(tag.length));
+};
+
+//проверка валидности хештега
+// const hasValidHashtag = () => {
+
+// }
+
+//проверка по количеству хештегов
+const hasValidCount = (value) => {
+  normalizeHashtag(value).length <= MAX_COUNT_HASHTAGS;
+};
+
+//проверка на уникальность хештегов
+
+
+//добавление валидатора полю хештег
+pristine.addValidator(
+  hashtagsField,
+  hasValidCount,
+  COUNT_HASHTAG_ERROR_MESSAGE,
+  3,
+  false
+);
+
+form.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
 });
