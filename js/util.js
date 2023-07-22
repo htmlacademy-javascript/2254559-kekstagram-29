dadc833e30e8b78156d58788a3552cc35e2797e3
Александр Головко <a-golovko2009@yandex.ru>
2023-07-22 12:33:50 +0500

    Удаляет код генерации случайных данных

--- js/util.js
+++ js/util.js
@@ -1,34 +1,4 @@
-import {NUMBER_PHOTOS_ON_PAGE} from './data.js';
-
 const ALERT_SHOW_TIME = 5000;
-
-// Функция для генерации случайного числа в заданном диапазоне значений.
-const getRandomInteger = (min, max) => {
-  const result = Math.random() * (max - min + 1) + min;
-  return Math.floor(result);
-}
-
-//Функция для генерации случайного числа без повторений из диапазона
-const getRandomIdFromRange = (min, max) => {
-  const previousValues = [];
-
-  return function () {
-    let currentValue = getRandomInteger(min, max);
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
-  };
-};
-
-// создание массивов случайных чисел в заданном диапазоне
-const arrayRandomIdPhoto = Array.from({length: 25}, getRandomIdFromRange(1,NUMBER_PHOTOS_ON_PAGE));
-const arrayRandomIdAddress = Array.from({length: 25}, getRandomIdFromRange(1,NUMBER_PHOTOS_ON_PAGE));
-
-// функция для выбора случайного элемента массива
-const getrandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
 
 //функция показа сообщения с ошибкой
 const showAlert = (message) => {
@@ -53,4 +23,4 @@ const showAlert = (message) => {
 }
 
 
-export {arrayRandomIdPhoto, arrayRandomIdAddress, getrandomArrayElement, getRandomInteger};
+export {showAlert};
